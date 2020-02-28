ceebe664964b42c4f18bd9e7a9434b74c00ce154
lisheng-test <18846425647@163.com>
2020-02-28 15:08:20 +0800

    leetcode 217

---
+++ LeetCode-217.cpp
@@ -0,0 +1,44 @@
+/*************************************************************************
+	> File Name: LeetCode-217.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2020年02月28日 星期五 15时07分48秒
+ ************************************************************************/
+
+#define swap(a, b) {\
+    __typeof(a) __temp = a;\
+    a = b; b = __temp;\
+}
+
+void merge_sort(int *num, int l, int r) {
+    if (r - l <= 1) {
+        if (r - l == 1 && num[l] > num[r]) {
+            swap(num[l], num[r]);
+        }
+        return ;
+    }
+    int mid = (l + r) >> 1;
+    merge_sort(num, l, mid);
+    merge_sort(num, mid + 1, r);
+    int *temp = (int *)malloc(sizeof(int) * (r - l + 1));
+    int p1 = l, p2 = mid + 1, k = 0;//递归回溯过程中每个小数组的顺序已经排好了
+    while (p1 <= mid || p2 <= r) {//将两个数组合并成新的数组的过程
+        if (p2 > r || (p1 <= mid && num[p1] <= num[p2])) {
+            temp[k++] = num[p1++];
+        } else {
+            temp[k++] = num[p2++];
+        }
+    }
+    memcpy(num + l, temp, sizeof(int) * (r - l + 1));
+    free(temp);
+    return ;
+}
+
+
+bool containsDuplicate(int* nums, int numsSize){
+    merge_sort(nums, 0, numsSize - 1);
+    for (int i = 0; i < numsSize - 1; i++) {
+        if (nums[i] == nums[i + 1]) return true;
+    }
+    return false;
+}
