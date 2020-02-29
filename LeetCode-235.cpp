34decf63b864d69ae74dc7b531dc565b2a2991da
lisheng-test <18846425647@163.com>
2020-02-29 19:35:21 +0800

    leetcode 235

---
+++ LeetCode-235.cpp
@@ -0,0 +1,40 @@
+/*************************************************************************
+	> File Name: LeetCode-235.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2020年02月29日 星期六 19时34分56秒
+ ************************************************************************/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+/*struct TreeNode *__lowestCommonAncestor(struct TreeNode *root, struct TreeNode *p, struct TreeNode *q) {
+    if (root->val == p->val) return p;
+    if (root->val == q->val) return q;
+    if (root->val > p->val && root->val < q->val) return root;
+    if (p->val > root->val) return __lowestCommonAncestor(root->right, p, q);
+    return __lowestCommonAncestor(root->left, p, q);
+}
+
+struct TreeNode* lowestCommonAncestor(struct TreeNode* root, struct TreeNode* p, struct TreeNode* q) {
+    if (root == NULL) return NULL;
+    if (p->val == q->val) return p;
+    if (p->val > q->val) {
+        struct TreeNode *temp = p;
+        p = q;
+        q = temp;
+    }//保证p是小的
+    return __lowestCommonAncestor(root, p, q);
+}*/
+
+struct TreeNode *lowestCommonAncestor(struct TreeNode *root, struct TreeNode *p, struct TreeNode *q){
+    if (root == NULL) return NULL;
+    if (p->val < root->val && q->val < root->val) return lowestCommonAncestor(root->left, p, q);
+    if (p->val > root->val && q->val > root->val) return lowestCommonAncestor(root->right, p, q);
+    else return root;
+}
